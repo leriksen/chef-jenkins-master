7bcf43eb3d26f98a999b9d947cac9959106fcead
Leif Eriksen <leif.eriksen.au@gmail.com>
2024-01-11 16:21:16 +1100

    add nsg rule to allow inbound to jenkins port

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,3 +18,21 @@ provider "azurerm" {
 
 data "azurerm_client_config" "current" {}
 
+data azurerm_network_security_group "nsg" {
+  name = "jenkins-master-vagrantNSG"
+  resource_group_name = "testing"
+}
+
+resource azurerm_network_security_rule "allowIn8080" {
+  name                        = "allowIn8080"
+  priority                    = 1000
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "8080"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = "testing"
+  network_security_group_name = data.azurerm_network_security_group.nsg.name
+}
