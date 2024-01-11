1a1976010f131710bc9b8cc2838cf687b293d90f
Leif Eriksen <leif.eriksen.au@gmail.com>
2024-01-11 14:00:12 +1100

    install jenkins

--- cookbooks/jenkins-master/recipes/jenkins-master-install.rb
+++ cookbooks/jenkins-master/recipes/jenkins-master-install.rb
@@ -1,7 +1,7 @@
-execute "apt-get-update" do
+execute 'apt-get-update' do
   command "apt-get update"
-  ignore_failure true
 end
+
 package 'fontconfig' do
   action :install
 end
@@ -9,3 +9,23 @@ end
 package 'openjdk-11-jre' do
   action :install
 end
+
+execute 'install-jenkins-apt-repo-key' do
+  command 'wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key'
+end
+
+file '/etc/apt/sources.list.d/jenkins.list' do
+  content <<~END
+    deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/
+  END
+end
+
+# need to update after changing apt sources
+execute 'apt-get-update' do
+  command "apt-get update"
+end
+
+package 'jenkins' do
+  action :install
+end
+
